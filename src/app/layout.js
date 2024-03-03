e2bbd8d2f3e4123a07fa85dd7c45ae64b1f7abd6
Ismoiljon Abdulloev <2022abdulloev@gmail.com>
2024-03-04 11:49:57 +1300

    main update

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,24 +17,26 @@ export default function RootLayout({ children }) {
     <div className="h-40-r w-40-r bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
     <div className="h-35-r w-35-r bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full absolute top-96 -left-20 transform rotate-180 animate-pulse"></div>
     <div className="h-12 w-full bg-white bg-opacity-10 shadow-5xl relative z-20 backdrop-filter backdrop-blur-sm flex items-center justify-between px-2 text-white">
-      <div>LOGO <span>Vacancies App</span></div>
+      <div>LOGO <span>Кор</span></div>
       <div>
         <nav>
           <ul className="flex space-x-2">
             <li>
-              Асоси
+              Чустучуи кор
             </li>
             <li>
-              Дар бораи мо
+              Руйхати корхо
             </li>
             <li>
-              Даромад
+            Музди кор
             </li>
           </ul>
         </nav>
       </div>
   </div>
+  <div className="relative z-20 ">
         {children}
+  </div>
   </div>
         </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,27 @@ export default function Home() {
   const title = "Vacancies App"
 
   return (
-    <div className="container h-96 w-full m-20 bg-white bg-opacity-10 rounded-2xl shadow-5xl relative z-20 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
-  <div className="text-white">Hello world</div>
-  </div>
+    <div className="z-30 text-white w-full h-screen"> 
+    <div className="flex flex-col h-32 w-full p-5 justify-center space-y-4 font-light" >
+      <p className="text-5xl">
+        <span className="text-blue-500">Имрӯз </span> кори нави худро пайдо кунед
+      </p>
+      <span className="text-xl">
+      Ҳазорҳо ҷойҳои корӣ дар бахшҳои компютерӣ, муҳандисӣ ва технология шуморо интизоранд.
+      </span>
+    </div>
+
+    <div className="w-full h-full grid grid-cols-[1fr_3fr_1fr] gap-3 justify-between p-2">
+    <div className="h-full bg-white bg-opacity-10 shadow-5xl relative z-20 backdrop-filter backdrop-blur-sm flex justify-between px-2 text-white">
+      <p>Фильтр</p>
+    </div>
+    <div className="h-full bg-white bg-opacity-10 shadow-5xl relative z-20 backdrop-filter backdrop-blur-sm flex justify-between px-2 text-white">
+      <p>Корхо</p>
+    </div>
+    <div className="h-full bg-white bg-opacity-10 shadow-5xl relative z-20 backdrop-filter backdrop-blur-sm flex justify-between px-2 text-white">
+      <p>Notification</p>
+    </div>
+    </div>
+    </div>
   );
 }
